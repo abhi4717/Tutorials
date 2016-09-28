06546639a306a431005704a033e6713aa3ac18b1
Abhishek Gopalan <abhi4717@gmail.com>
2016-09-28 09:26:28 +0530

    Chapter 3 Views: Viewbag example

---
+++ MVC Tutorial/Controllers/Chapter3ViewController.cs
@@ -0,0 +1,16 @@
+﻿using MVC_Tutorial.ViewModel;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVC_Tutorial.Controllers
+{
+    public class Chapter3ViewController : Controller
+    {
+        // GET: Chapter3View
+        public ActionResult Index()
+        {
+            ViewBag.Artist = new List<Album>() { new ViewModel.Album() { Artist = "Artist 1", Price = 10, Title = "Test Title 1" } };
+            return View();
+        }
+    }
+}
